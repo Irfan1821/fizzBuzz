2d80f3ef4fa46d585bfb149ff6cbc996c46c921f
Irfanm <irfani847000@gmail.com>
2023-04-13 18:10:19 +0530

    second commit

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-function fizzBuzz(phone) {
-  const phoneNumber = phone
+let output = "";
+const fizzBuzz = (phoneNumber) => {
+  const phonNumberSum = phoneNumber
     .toString()
     .split("")
     .reduce((sum, digit) => sum + parseInt(digit), 0);
-  for (let i = 1; i <= phoneNumber; i++) {
+
+  for (let i = 1; i <= phonNumberSum; i++) {
     if (i % 4 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
+      output += "FizzBuzz ";
     } else if (i % 4 === 0) {
-      console.log("Fizz");
+      output += "Fizz ";
     } else if (i % 5 === 0) {
-      console.log("Buzz");
+      output += "Buzz ";
     } else {
-      console.log(i);
+      output = output + i + " ";
     }
   }
-}
+  console.log(output);
+  return output;
+};
+fizzBuzz(8700052375);
